804b90c96cd8bb7d58c2f38dc3de6f7ab79888ca
rahul singh <rsinghsep@gmail.com>
2021-05-01 14:24:48 -0500

    added fake wait on splash activity

--- app/src/main/java/com/staysilly/socialdistancingapp/views/SplashActivity.java
+++ app/src/main/java/com/staysilly/socialdistancingapp/views/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.staysilly.socialdistancingapp.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 import com.staysilly.socialdistancingapp.R;
 
@@ -21,6 +23,26 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        fakeWait();
+    }
+
+
+    /*/////////////////////////////////////////////////
+    //PRIVATE METHODS
+    /*/////////////////////////////////////////////////
+    private void startHomeActivity(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+    private void fakeWait(){
+        new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {}
+            @Override
+            public void onFinish() {
+                startHomeActivity();
+            }
+        }.start();
     }
 
 }
