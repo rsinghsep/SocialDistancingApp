260cabc62d4f31d66fece547e6d927bc88b69833
rahul singh <rsinghsep@gmail.com>
2021-05-01 13:19:38 -0500

    added empty activity for splash and home activity

---
+++ app/src/main/java/com/staysilly/socialdistancingapp/views/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.staysilly.socialdistancingapp.views;
+
+import android.os.Bundle;
+
+import com.staysilly.socialdistancingapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/staysilly/socialdistancingapp/views/SplashActivity.java
+++ app/src/main/java/com/staysilly/socialdistancingapp/views/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.staysilly.socialdistancingapp;
+package com.staysilly.socialdistancingapp.views;
 
 import android.os.Bundle;
 
+import com.staysilly.socialdistancingapp.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
