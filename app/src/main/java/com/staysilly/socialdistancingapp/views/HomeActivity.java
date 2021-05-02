bf6acb7dafa5635cf0e4d70c5d2d66b1a413d80e
rahul singh <rsinghsep@gmail.com>
2021-05-02 01:34:53 -0500

    moving location to last known location

--- app/src/main/java/com/staysilly/socialdistancingapp/views/HomeActivity.java
+++ app/src/main/java/com/staysilly/socialdistancingapp/views/HomeActivity.java
@@ -1,17 +1,35 @@
 package com.staysilly.socialdistancingapp.views;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.staysilly.socialdistancingapp.R;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     /*/////////////////////////////////////////////////
     //MEMBERS
     /*/////////////////////////////////////////////////
-    private final String TAG = this.getClass().getSimpleName();
+    private final String TAG = "**" + this.getClass().getSimpleName();
+    private GoogleMap googleMap;
+    private FusedLocationProviderClient fusedLocationClient;
 
 
     /*/////////////////////////////////////////////////
@@ -21,6 +39,66 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+
+    /*/////////////////////////////////////////////////
+    //PRIVATE METHODS
+    /*/////////////////////////////////////////////////
+    private void moveMapToLastKnownLocation(final GoogleMap map) {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(this, "provide location permission", Toast.LENGTH_LONG).show();
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                Log.d(TAG, "found last known location");
+                if (map!=null){
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    CameraPosition myPosition = new CameraPosition.Builder().target(latLng).zoom(16).bearing(90).tilt(30).build();
+                    map.animateCamera(CameraUpdateFactory.newCameraPosition(myPosition));
+                }
+            }
+        });
+    }
+
+
+    /*/////////////////////////////////////////////////
+    //Interface overrides
+    /*/////////////////////////////////////////////////
+    @Override
+    public void onMapReady(@NonNull GoogleMap map) {
+        Log.d(TAG, "map is ready");
+        googleMap = map;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d(TAG, "ACCESS_FINE_LOCATION permission needed");
+            Toast.makeText(this, "provide location permission", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Log.d(TAG, "permission is granted");
+        googleMap.setMyLocationEnabled(true);
+        moveMapToLastKnownLocation(googleMap);
     }
 
 }
