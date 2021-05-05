48d49142d2987bf05406e49225f5c3d305f0054b
rahul singh <rsinghsep@gmail.com>
2021-05-05 02:25:43 -0500

    added dependency for firebase firestore and saving dummy data

--- app/src/main/java/com/staysilly/socialdistancingapp/models/Location.java
+++ app/src/main/java/com/staysilly/socialdistancingapp/models/Location.java
@@ -9,5 +9,47 @@ public class Location {
     private String userId;
     private double latitude;
     private double longitude;
+    private boolean isOnline;
+
+
+    /*/////////////////////////////////////////////////
+    //PROPERTY
+    /*/////////////////////////////////////////////////
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+    public boolean isOnline() {
+        return isOnline;
+    }
+    public void setOnline(boolean online) {
+        isOnline = online;
+    }
+
+
+    /*/////////////////////////////////////////////////
+    //CONSTRUCTOR
+    /*/////////////////////////////////////////////////
+    public Location(String id, double latitude, double longitude, boolean isOnline){
+        this.userId = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.isOnline = isOnline;
+    }
+    public Location(){}
 
 }
--- app/src/main/java/com/staysilly/socialdistancingapp/repository/AppRepository.java
+++ app/src/main/java/com/staysilly/socialdistancingapp/repository/AppRepository.java
@@ -1,10 +1,31 @@
 package com.staysilly.socialdistancingapp.repository;
 
+import android.content.Context;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.staysilly.socialdistancingapp.models.Location;
+
 public class AppRepository {
 
     /*/////////////////////////////////////////////////
     //MEMBERS
     /*/////////////////////////////////////////////////
     private final String TAG = this.getClass().getSimpleName();
+    private static final String USER_LOCATION_COLLECTION = "USER_LOCATION_COLLECTION";
+
+
+    /*/////////////////////////////////////////////////
+    //PRIVATE METHODS
+    /*/////////////////////////////////////////////////
+    public static void setValue(Context context){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = database.collection(USER_LOCATION_COLLECTION);
+
+        Location location = new Location("16582374-234723-2354823-75234", 12721.232, 432432.2123, true);
+
+        collectionReference.document()
+                .set(location);
+    }
 
 }
